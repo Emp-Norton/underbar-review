8e0107b423400d199f21eaf6ec8d89568ab2b437
James Jelenko <jamesjelenko@gmail.com>
2017-11-28 11:41:24 -0800

    progress up through uniq.

--- src/underbar.js
+++ src/underbar.js
@@ -7,6 +7,7 @@
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+    return val;
   };
 
   /**
@@ -37,6 +38,14 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    var results = [];
+    if (n === 0) return [];
+    if (!n) return array[array.length - 1];
+    if (n >= array.length) return array;
+    for (var idx = array.length - n; idx < array.length; idx++){
+      results.push(array[idx]);
+    }
+    return results;
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -45,6 +54,15 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if (Array.isArray(collection)) {
+      for (var idx = 0; idx < collection.length; idx++){
+        iterator(collection[idx], idx, collection);
+      }
+    } else if (typeof collection === "object") {
+      for (var key in collection) {
+        iterator(collection[key], key, collection);
+      }
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
@@ -66,16 +84,67 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var results = [];
+    _.each(collection, function(element){
+      if (test(element)){
+        results.push(element)
+      }
+    });
+    return results;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
-    // TIP: see if you can re-use _.filter() here, without simply
-    // copying code in and modifying it
+    var results = _.filter(collection, function(element){
+      return !test(element)
+    });
+    return results
   };
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array, isSorted, iterator) {
+    var iteratedUniqs = [];
+    var final = [];
+    var uniqs = [];
+
+    var findUniqs = function(array){
+      var uniqs = [];
+      for (var idx = 0; idx < array.length; idx++){
+        if (array[idx][0]){ 
+            if (!uniqs.includes(array[idx][0])) uniqs.push(array[idx])
+        } else {
+          if (!uniqs.includes(array[idx])) uniqs.push(array[idx])
+        }
+      
+      }
+      console.log("uniqs" + uniqs)
+    return uniqs;
+    }
+
+    if (isSorted){ 
+      if (iterator) { // sorted, iterator
+        var iterated = array.map(function(element, index){
+          return [iterator(element), index]
+        });
+       // console.log(iterated)
+        var uniqIndices = findUniqs(iterated)
+        //console.log(uniqIndices)
+       
+      } else { // sorted, no iterator
+        for (var idx  = 0; idx < array.length; idx++){
+          if (array[idx + 1]){
+            if (array[idx] != array[idx + 1]){ 
+              uniqs.push(array[idx])
+            }
+          } else {
+            uniqs.push(array[array.length-1])
+          }
+        }
+      }
+    } else { // unsorted, no iterator
+      return findUniqs(array);
+    }
+    return iteratedUniqs;
   };
 
 
